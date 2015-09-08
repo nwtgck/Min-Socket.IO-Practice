f4f5d0744007e5a1bfb928d8b21cfacae2a2b0db
Ryo Ota <nwtgck@gmail.com>
2015-09-08 19:28:21 +0900

    [change] replace spaces with tabs

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+	res.sendfile('index.html');
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
 
 io.sockets.on('connection', function (socket){
-  // hello, worldはクライアントが接続するとすぐに1度だけ送信されます
-  socket.emit('message_from_server', 'hello, world');
+	// hello, worldはクライアントが接続するとすぐに1度だけ送信されます
+	socket.emit('message_from_server', 'hello, world');
 
-  //　クライアントからmessage_from_clientがemitされた時
-  socket.on('message_from_client', function (msg){
-    console.log('message:', msg);
-  });
+	//　クライアントからmessage_from_clientがemitされた時
+	socket.on('message_from_client', function (msg){
+		console.log('message:', msg);
+	});
 });
