5cd0257b07bf4bd0d583d31cddc540af18575d40
Ryo Ota <nwtgck@gmail.com>
2015-09-08 18:19:12 +0900

    [add] first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+io.sockets.on('connection', function (socket){
+  // hello, worldはクライアントが接続するとすぐに1度だけ送信されます
+  socket.emit('message_from_server', 'hello, world');
+
+  //　クライアントからmessage_from_clientがemitされた時
+  socket.on('message_from_client', function (msg){
+    console.log('message:', msg);
+  });
+});
